3d9ed493609c25f95bfa008d48a83c8f2df69290
rosen <tranhuyducseven@gmail.com>
2021-10-22 20:56:26 +0700

    full event

--- Lab02/1911071/phan2_bai3/main.js
+++ Lab02/1911071/phan2_bai3/main.js
@@ -2,54 +2,76 @@ function Validator(options) {
   // Get form
   var formElement = document.querySelector(options.form);
   if (formElement) {
-    options.rules.forEach(function(rule) {
+    options.rules.forEach(function (rule) {
       var inputElement = formElement.querySelector(rule.selector);
       if (inputElement) {
-        inputElement.onblur = function() {
+        inputElement.onblur = function () {
           validate(inputElement, rule);
-        }
+        };
       }
-    })
+    });
   }
+}
 
-}
 //define rules
-Validator.isRequired = function(selector) {
+Validator.isValidLength = function (selector) {
   return {
     selector: selector,
-    test: function(value) {
-      return value.trim() ? undefined : "What is your name?";
-    }
-  }
-}
-Validator.isEmail = function(selector) {
+    test: function (value) {
+      return value.length > 1 && value.length < 31
+        ? undefined
+        : "Please enter minimum 2 characters and maximum 30 characters";
+    },
+  };
+};
+Validator.isEmail = function (selector) {
   return {
     selector: selector,
-    test: function(value) {
+    test: function (value) {
       const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-      return regex.test(value)? undefined:"You will use this when you log in and if you ever need to reset password."
-    }
-  }
-}
-Validator.isPassword = function(selector){
+      return regex.test(value)
+        ? undefined
+        : "You will use this when you log in and if you ever need to reset password.";
+    },
+  };
+};
+Validator.isRequiredBirthday = function (selector) {
   return {
     selector: selector,
-    test: function(value) {
-      return value ? undefined : "What is your password?";
-    }
-  }
-}
-Validator.isValidLength = function (selector){
+    test: function (value) {
+      return value && parseInt(value) > 0
+        ? undefined
+        : "Please chose your birthday!";
+    },
+  };
+};
+Validator.isRequiredCountry = function (selector) {
   return {
-    selector:selector,
-    test: function(value){
-      return value.length > 1 && value.length < 31?undefined: "Enter at least 2 characters and maximum 30 characters"
-    }
-  }
-}
+    selector: selector,
+    test: function (value) {
+      var genderInputs = document.querySelectorAll(".form-check-input[name='gender']");
+      if(!genderInputs[0].checked&&!genderInputs[1].checked&&!genderInputs[2].checked){
+        alert("You must select your gender!")
+      }
+      return value && parseInt(value) > 0
+        ? undefined
+        : "Please chose your country!";
+    },
+  };
+};
+Validator.isValidAbout = function (selector) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return  value.length < 1001
+        ? undefined
+        : "Your paragraph is limited to 1000 characters";
+    },
+  };
+};
+
 //Alert Error
 function validate(inputElement, rule) {
   var errorMessage = rule.test(inputElement.value);
-  if (errorMessage)
-    alert(errorMessage);
+  if (errorMessage) alert(errorMessage);
 }
