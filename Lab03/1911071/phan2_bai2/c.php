089e2453406385ca25dea6710d97f1b328433f24
tranhuyducseven <tranhuyducseven@gmail.com>
2021-11-05 15:17:36 +0700

    title name

--- Lab03/1911071/phan2_bai2/c.php
+++ Lab03/1911071/phan2_bai2/c.php
@@ -54,7 +54,7 @@ if ($idGet) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <title>Car - Delete Tran Huy Duc 1911071</title>
+    <title>Car - Update Tran Huy Duc 1911071</title>
     <style>
         .form-control {
             margin: 10px 0 20px;
