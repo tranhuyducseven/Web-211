f4b162453bc586236521d6d9d2ae435602f66623
rosen <you@example.com>
2021-11-19 14:46:05 +0700

    add http status

--- Lab04/1911071/1911071/phan2_bai2/a.php
+++ Lab04/1911071/1911071/phan2_bai2/a.php
@@ -1,10 +1,15 @@
  <?php
-  require_once('database.php');
-  header('Access-Control-Allow-Origin:*');
-  ?>
+   require_once('database.php');
+   header('Access-Control-Allow-Origin:*');
+   header('Content-Type: application/json');
+   ?>
  <?php
-  $sql = "SELECT * FROM cars";
-  $result = executeResult($sql);
-  echo json_encode($result);
-  ?>
-   
+   if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+      $json = array();
+      $sql = "SELECT * FROM cars";
+      $result = executeResult($sql);
+      $json['data'] = $result;
+      $json['message'] = 'Successfully get the information of cars!';
+      $json['status'] = 200;
+      echo json_encode($json);
+   }
--- Lab04/1911071/1911071/phan2_bai2/main.js
+++ Lab04/1911071/1911071/phan2_bai2/main.js
@@ -1,156 +1,186 @@
-$(document).ready(function() {
-    //---------------------LOAD DATA
-    function viewData() {
-        $.get("./a.php", function(data) {
-            $("#load-data").html(render(JSON.parse(data)));
+$(document).ready(function () {
+  //---------------------LOAD DATA
+  function viewData() {
+    $.ajax({
+      url: "./a.php",
+      type: "GET",
+      success: function (response, status) {
+        if (status == "success" && response) {
+          $("#load-data").html(render(response.data));
+          alert("Status: " + response.status + "\r\n" + response.message);
+        } else {
+          alert("Error: Can't read data!")
+        }
+      },
+      error: function  (jqXHR, exception) {
+        var msg = "";
+        if (jqXHR.status === 0) {
+          msg = "Not connect.\n Verify Network.";
+        } else if (jqXHR.status == 404) {
+          msg = "Requested page not found. [404]";
+        } else if (jqXHR.status == 500) {
+          msg = "Internal Server Error [500].";
+        } else if (exception === "parse error") {
+          msg = "Requested JSON parse failed.";
+        } else if (exception === "timeout") {
+          msg = "Time out error.";
+        } else if (exception === "abort") {
+          msg = "Ajax request aborted.";
+        } else {
+          msg = "Uncaught Error.\n" + jqXHR.responseText;
+        }
+        alert(msg);
+      },
+    });
+  }
+  $("#show-btn").click(function () {
+    viewData();
+  });
+  //---------------------ADD DATA
+
+  $("#show-form-btn").click(function () {
+    $("#add-form").toggle();
+  });
+  $("#add-btn").on("click", function () {
+    var id = $("#car-id").val();
+    var name = $("#car-name").val();
+    var year = $("#car-year").val();
+    var check = true;
+    if (id == "") alert("Please enter the car id");
+    if (name == "") alert("Please enter the car name");
+    if (year == "") alert("Please enter the car year");
+    if (id != "" && name != "" && year != "") {
+      if (!Number.isInteger(Number(id))) {
+        alert("Please enter id with integer type");
+        check = false;
+      }
+      if (name.length < 5 || name.length > 40) {
+        alert("Please enter string type with length 5-40 characters");
+        check = false;
+      }
+      if (
+        !Number.isInteger(Number(year)) ||
+        Number(year) < 1990 ||
+        Number(year) > 2015
+      ) {
+        alert("Please enter year from 1995 to 2015");
+        check = false;
+      }
+      if (check) {
+        $.ajax({
+          url: "./b.php",
+          type: "POST",
+          data: JSON.stringify({
+            id: id,
+            name: name,
+            year: year,
+          }),
+          contentType: "application/json",
+          success: function () {
+            alert("Successfully insert data to database");
+            $("#insert-form")[0].reset();
+            $("#add-form").show();
+            viewData();
+          },
         });
+      }
     }
+  });
+  //---------------------EDIT DATA
 
-    $("#show-btn").click(function() {
-        viewData();
-    });
+  $("#show-update-btn").click(function () {
+    $("#edit-form").toggle();
+  });
+  var curID = 0;
+  $(document).on("click", ".edit-btn", function () {
+    curID = $(this).attr("value");
+    $("#car-id-edit").val(curID);
+  });
 
-    //---------------------ADD DATA
+  $("#update-btn").on("click", function () {
+    var id = $("#car-id-edit").val();
+    var name = $("#car-name-edit").val();
+    var year = $("#car-year-edit").val();
 
-    $("#show-form-btn").click(function() {
-        $("#add-form").toggle();
-    });
-    $("#add-btn").on("click", function() {
-        var id = $("#car-id").val();
-        var name = $("#car-name").val();
-        var year = $("#car-year").val();
-        var check = true;
-        if (id == "") alert("Please enter the car id");
-        if (name == "") alert("Please enter the car name");
-        if (year == "") alert("Please enter the car year");
-        if (id != "" && name != "" && year != "") {
-            if (!Number.isInteger(Number(id))) {
-                alert("Please enter id with integer type");
-                check = false;
-            }
-            if (name.length < 5 || name.length > 40) {
-                alert("Please enter string type with length 5-40 characters");
-                check = false;
-            }
-            if (
-                !Number.isInteger(Number(year)) ||
-                Number(year) < 1990 ||
-                Number(year) > 2015
-            ) {
-                alert("Please enter year from 1995 to 2015");
-                check = false;
-            }
-            if (check) {
-                $.ajax({
-                    url: "./b.php",
-                    type: "POST",
-                    data: JSON.stringify({
-                        id: id,
-                        name: name,
-                        year: year,
-                    }),
-                    contentType: "application/json",
-                    success: function() {
-                        alert("Successfully insert data to database");
-                        $("#insert-form")[0].reset();
-                        $("#add-form").show();
-                        viewData();
-                    },
-                });
-            }
+    var check = true;
+    if (id == "") alert("Please enter the car id");
+    if (name == "") alert("Please enter the car name");
+    if (year == "") alert("Please enter the car year");
+    if (id != "" && name != "" && year != "") {
+      if (!Number.isInteger(Number(id))) {
+        alert("Please enter id with integer type");
+        check = false;
+      }
+      if (name.length < 5 || name.length > 40) {
+        alert("Please enter string type with length 5-40 characters");
+        check = false;
+      }
+      if (
+        !Number.isInteger(Number(year)) ||
+        Number(year) < 1990 ||
+        Number(year) > 2015
+      ) {
+        alert("Please enter year from 1995 to 2015");
+        check = false;
+      }
+      if (check) {
+        var msgConfirm =
+          curID === id
+            ? "Are you sure to edit the data of this car?"
+            : "Are you sure to edit this car with id: " +
+              curID +
+              " to " +
+              id +
+              " ?";
+        var checkConfirm = confirm(msgConfirm);
+        if (checkConfirm) {
+          $.ajax({
+            url: "./c.php",
+            type: "PUT",
+            data: JSON.stringify({
+              curID: curID,
+              id: id,
+              name: name,
+              year: year,
+            }),
+            contentType: "application/json",
+
+            success: function () {
+              alert("Successfully edit data ");
+              $("#update-form")[0].reset();
+              $("#edit-form").show();
+              viewData();
+            },
+          });
         }
-    });
-    //---------------------EDIT DATA
+      }
+    }
+  });
 
-    $("#show-update-btn").click(function() {
-        $("#edit-form").toggle();
-    });
-    var curID = 0;
-    $(document).on("click", ".edit-btn", function() {
-        curID = $(this).attr("value");
-        $("#car-id-edit").val(curID);
-    });
+  //---------------------REMOVE DATA
 
-    $("#update-btn").on("click", function() {
-        var id = $("#car-id-edit").val();
-        var name = $("#car-name-edit").val();
-        var year = $("#car-year-edit").val();
-
-        var check = true;
-        if (id == "") alert("Please enter the car id");
-        if (name == "") alert("Please enter the car name");
-        if (year == "") alert("Please enter the car year");
-        if (id != "" && name != "" && year != "") {
-            if (!Number.isInteger(Number(id))) {
-                alert("Please enter id with integer type");
-                check = false;
-            }
-            if (name.length < 5 || name.length > 40) {
-                alert("Please enter string type with length 5-40 characters");
-                check = false;
-            }
-            if (
-                !Number.isInteger(Number(year)) ||
-                Number(year) < 1990 ||
-                Number(year) > 2015
-            ) {
-                alert("Please enter year from 1995 to 2015");
-                check = false;
-            }
-            if (check) {
-                var msgConfirm =
-                    (curID === id) ?
-                    "Are you sure to edit the data of this car?" :
-                    "Are you sure to edit this car with id: " + curID + " to " + id + " ?";
-                var checkConfirm = confirm(msgConfirm);
-                if (checkConfirm) {
-                    $.ajax({
-                        url: "./c.php",
-                        type: "PUT",
-                        data: JSON.stringify({
-                            curID: curID,
-                            id: id,
-                            name: name,
-                            year: year,
-                        }),
-                        contentType: "application/json",
-
-                        success: function() {
-                            alert("Successfully edit data ");
-                            $("#update-form")[0].reset();
-                            $("#edit-form").show();
-                            viewData();
-                        },
-                    });
-                }
-            }
-        }
-    });
-
-    //---------------------REMOVE DATA
-
-    $(document).on("click", ".del-btn", function() {
-        var id = $(this).attr("value");
-        var check = confirm("Are you sure to delete this car?");
-        if (check) {
-            $.ajax({
-                url: "./d.php",
-                type: "DELETE",
-                data: JSON.stringify({ id: id }),
-                success: function() {
-                    alert("Successfully  remove a car!");                    
-                    viewData();
-                },
-            });
-        }
-    });
+  $(document).on("click", ".del-btn", function () {
+    var id = $(this).attr("value");
+    var check = confirm("Are you sure to delete this car?");
+    if (check) {
+      $.ajax({
+        url: "./d.php",
+        type: "DELETE",
+        data: JSON.stringify({ id: id }),
+        success: function () {
+          alert("Successfully  remove a car!");
+          viewData();
+        },
+      });
+    }
+  });
 });
 
 //---------------------RENDER DATA
 
 function render(cars) {
-    const cellGroup = cars.map((car, index) => {
-        return `<tr>
+  const cellGroup = cars.map((car, index) => {
+    return `<tr>
     <th scope="row"> ${++index}</th>
     <td class="data-cell">${car["id"]}</td>
     <td class="data-cell">${car["name"]}</td>
@@ -160,8 +190,8 @@ function render(cars) {
         <a class="btn btn-danger del-btn " value="${car["id"]}">Delete a car</a>
     </td>
     </tr> `;
-    });
-    const headTable =
-        '<div class="card-body"><table class="table table-striped table-hover"><thead><tr><th scope="col">NO</th><th scope="col">ID</th><th scope="col">NAME</th><th scope="col">YEAR</th></tr></thead><tbody>';
-    return headTable + cellGroup.join("");
+  });
+  const headTable =
+    '<div class="card-body"><table class="table table-striped table-hover"><thead><tr><th scope="col">NO</th><th scope="col">ID</th><th scope="col">NAME</th><th scope="col">YEAR</th></tr></thead><tbody>';
+  return headTable + cellGroup.join("");
 }
